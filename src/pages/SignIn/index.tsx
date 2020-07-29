c85d87d43150df98eb2a84f302d1c9610e0ef6c1
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-07-28 23:20:19 -0300

    Class finished: Importing external fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,8 +9,12 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
 
 export default SignIn;
+
+// I solved it by adding a new line to end of ios/[Project].xcodeproj/project.pbxproj
